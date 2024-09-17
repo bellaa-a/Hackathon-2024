bd0d1c2ff9d59354243d66fcfb7873e46775f34f
Bella Lu <bellalu@bellas-mbp-2.acasmart.jh.edu>
2024-09-17 12:27:44 -0400

    pnpm installed

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'app', // Set the root to the subfolder containing index.html
+  build: {
+    outDir: '../dist', // Ensure build output goes to the parent directory
+    emptyOutDir: true, // Optional: Clean the output directory before each build
+  },
+});
